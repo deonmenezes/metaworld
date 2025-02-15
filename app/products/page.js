a949680eb9bf89268cc7046b942267075bfa09d1
deonmenezes <deonmenezescodes@gmail.com>
2025-02-15 14:49:53 +0530

    oiiii

--- app/products/page.js
+++ app/products/page.js
@@ -1,7 +1,8 @@
-import {ProductsGridcom} from "../../sections";
+import { ProductsGridcom } from '../../sections';
+
 const ProductsPage = () => (
   <div className="bg-primary-black overflow-hidden">
-    <main className="relative flex items-center justify-center min-h-screen">
+      <main className="relative flex items-center justify-center min-h-screen">
       <div className=" shadow-lg backdrop-blur-md  p-10 rounded-lg">
       <div className="p-6">
       <h1 className="text-2xl font-bold mb-6 text-white text-center">Our Products</h1>
